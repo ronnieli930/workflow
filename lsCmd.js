637ae4211a578e94d8abca8d51d7ee9a79d5084f
Ronnie LI <icatchaflyinmyhand@gmail.com>
2023-04-23 03:27:20 +0800

    list names of cmds only

--- lsCmd.js
+++ lsCmd.js
@@ -1,11 +1,16 @@
-import { useCrayon, require } from './helpers.js';
+import { useCrayon, require, Logging } from './helpers.js';
 const { scripts } = require('./package.json');
 
 (() => {
   const {red, cyan, yellow, underscore} = useCrayon()
   const args = process.argv
-  // TODO
   // -n names only, don't show detail cmds
+  if (args.includes('-n')) {
+    Object.keys(scripts).forEach(e => {
+      console.log(cyan(e))
+    })
+    process.exit(0)
+  }
   const keys = args.length > 2 ? process.argv.slice(2) : Object.keys(scripts)
   keys.forEach(k => {
     if (!scripts[k]) {
